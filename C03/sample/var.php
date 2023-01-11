14c824e7f3fef995609700bfe54298c9f517dce4
tron01 <69385258+tron01@users.noreply.github.com>
2023-01-10 21:24:50 -0800

    c03 samples

---
+++ C03/sample/var.php
@@ -0,0 +1,16 @@
+<?php
+ 
+ echo" <h1>hello </h1>";
+ print " <p>Lorem ipsum, dolor sit amet consibus voluptas dolti officia aboriosam atque aacilis.</p> ";
+/*
+ $a ="Twinkle";
+
+$b="Star";
+
+echo $a ," " ,$a," little ", $b ,"<br> ";
+$a ="hello";
+
+$b="world";
+echo $a ," ", $b ;
+*/
+ ?>
